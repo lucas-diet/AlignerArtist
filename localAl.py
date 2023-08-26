ba3f656f054ebcfbe83e03648f4294e15a6eb138
lucas-diet <lucas.d1@gmx.net>
2023-08-26 19:15:57 +0200

    berecchnung für localeAl hinzugefügt

--- localAl.py
+++ localAl.py
@@ -1,6 +1,61 @@
+from fasta import Fasta as fasta
+from blosum62 import blosum62
 
 class SmithWaterman():
 
     def __init__(self, s1='',s2=''):
         self.s1 = s1
         self.s2 = s2
+        self.penalty = {'match':1, 'mismatch':-1, 'gap':-1}
+
+    def setPenalty(self, match,mismatch,gap):
+        self.penalty = {'match':match, 'mismatch':mismatch, 'gap':gap}
+
+    def getPenalty(self):
+        return self.penalty
+    
+    def similarities(self, c1,c2):
+        if c1 != c2:
+            if c1 == '-' or c2 == '-':
+                return self.penalty['gap']
+            elif c1 != '-' or c2 != '-':
+                return self.penalty['mismatch']
+        else:
+            return self.penalty['match']
+    
+    def initDP(self, s1,s2):
+        dp = [[0 for _ in range(len(s2)+1)] for _ in range(len(s1)+1)]
+        for i in range(0,len(s1)+1):
+            dp[i][0] = 0
+        for j in range(0,len(s2)+1):
+            dp[0][j] = 0
+        
+        return dp
+    
+    def calcualteDP(self, type,s1,s2):
+        dp = self.initDP(s1,s2)
+        if type == 'nt':
+            for i in range(1,len(s1)+1):
+                for j in range(1,len(s2)+1):
+                    diag = dp[i-1][j-1] + self.similarities(s1[i-1],s2[j-1])
+                    hori = dp[i][j-1] + self.similarities('-',s2[j-1])
+                    vert = dp[i-1][j] + self.similarities(s1[i-1],'-')
+                    dp[i][j] = max(0,diag,hori,vert)
+
+        elif type == 'aa':
+            for i in range(1,len(s1)+1):
+                for j in range(1,len(s2)+1):
+                    diag = dp[i-1][j-1] + blosum62[s1[i-1]][s2[j-1]]
+                    hori = dp[i][j-1] + self.similarities('-',s2[j-1])
+                    vert = dp[i-1][j] + self.similarities(s1[i-1],'-')
+                    dp[i][j] = max(0,diag,hori,vert)
+            
+        return dp
+    
+s1 = 'agt'
+s2 = 'agta'
+
+dp = SmithWaterman().calcualteDP('aa',s1,s2)
+
+for i in range(0,len(dp)):
+    print(dp[i])
