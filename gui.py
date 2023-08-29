e8b72569f7eb93ee902b235f76942fa589ea7a28
lucas-diet <lucas.d1@gmx.net>
2023-08-29 12:13:39 +0200

    gui weiterentwickelt bzgl neue fenster für tools

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 from fasta import Fasta
 from blosum62 import blosum62
@@ -12,7 +13,7 @@ class GuiApp(tk.Tk):
         self.title('Menu')
         self.geometry('300x200')
 
-        self.label = tk.Label(self, text='Choose a tool')
+        self.label = ttk.Label(self, text='Choose a tool')
         self.label.pack()
         
         self.options = [
@@ -23,6 +24,7 @@ class GuiApp(tk.Tk):
         self.tools = tk.StringVar()
         self.tools.set(self.options[0])
         self.drop = tk.OptionMenu(self, self.tools, *self.options)
+        self.drop.config(fg='black')
         self.drop.place(x=30,y=30)
 
         self.button = tk.Button(self, text='Apply', command=self.openToolWindow)
@@ -30,13 +32,42 @@ class GuiApp(tk.Tk):
     
     def openToolWindow(self):
         toolWindow = tk.Tk()
+        
+        sequenceOptions = ['Nucleotidsequence', 'Aminoacidsequence']
+        sequenceTypes = tk.StringVar(toolWindow)
+        sequenceTypes.set(sequenceOptions[0])
+
         if self.tools.get() == self.options[0]:
             toolWindow.title('Needlemann-Wunsch Algorithm')
-            toolWindow.geometry('500x500')
+            toolWindow.geometry('600x500')
+            
+            toolWindow.sequenceType = tk.OptionMenu(toolWindow, sequenceTypes, *sequenceOptions)
+            toolWindow.sequenceType.config(fg='black')
+            toolWindow.sequenceType.place(x=20,y=20)
+
+            toolWindow.seq1 = tk.Entry(toolWindow,width=50)
+            toolWindow.seq1.config(bg='white',fg='black')
+            toolWindow.seq1.place(x=60,y=80)
+
+            toolWindow.seq2 = tk.Entry(toolWindow,width=50)
+            toolWindow.seq2.config(bg='white',fg='black')
+            toolWindow.seq2.place(x=60,y=120)
         
         if self.tools.get() == self.options[1]:
             toolWindow.title('Smith-Waterman Algorithm')
-            toolWindow.geometry('500x500')
+            toolWindow.geometry('600x500')
+
+            toolWindow.sequenceType = tk.OptionMenu(toolWindow, sequenceTypes, *sequenceOptions)
+            toolWindow.sequenceType.config(fg='black')
+            toolWindow.sequenceType.place(x=20,y=20)
+
+            toolWindow.seq1 = tk.Entry(toolWindow,width=50)
+            toolWindow.seq1.config(bg='white',fg='black')
+            toolWindow.seq1.place(x=60,y=80)
+
+            toolWindow.seq2 = tk.Entry(toolWindow,width=50)
+            toolWindow.seq2.config(bg='white',fg='black')
+            toolWindow.seq2.place(x=60,y=120)
 
 if __name__ == '__main__':
     app = GuiApp()
